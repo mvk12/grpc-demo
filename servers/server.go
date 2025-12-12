cfb0c424b12108c8f19b864dcb3abf5346f91b5d
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2025-12-12 12:22:57 -0600

    feat: agregar servidor gRPC para el servicio Student

---
+++ servers/server.go
@@ -0,0 +1,45 @@
+package servers
+
+import (
+	"context"
+
+	"github.com/mvk12/grpc-demo/models"
+	"github.com/mvk12/grpc-demo/pb"
+	"github.com/mvk12/grpc-demo/repositories"
+)
+
+type Server struct {
+	repo repositories.Repository
+	pb.UnimplementedStudentServiceServer
+}
+
+func NewStudentServer(repo repositories.Repository) *Server {
+	return &Server{repo: repo}
+}
+
+func (s *Server) GetStudent(ctx context.Context, req *pb.GetStudentRequest) (*pb.Student, error) {
+	student, err := s.repo.GetStudentByID(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.Student{
+		Id:    student.ID,
+		Name:  student.Name,
+		Email: student.Email,
+	}, nil
+}
+
+func (s *Server) CreateStudent(ctx context.Context, req *pb.Student) (*pb.StudentResponse, error) {
+	student, err := s.repo.CreateStudent(ctx, &models.Student{
+		Name:  req.GetName(),
+		Email: req.GetEmail(),
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &pb.StudentResponse{
+		Id: student.ID,
+	}, nil
+}
