d94ac1365181d22591b016630c5abdec80383444
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2025-12-12 17:03:46 -0600

    feat: agregar estructura Question en el modelo con campos ID, Question, Answer y TestID

--- models/models.go
+++ models/models.go
@@ -11,3 +11,10 @@ type Test struct {
 	Title       string `json:"title"`
 	Description string `json:"description"`
 }
+
+type Question struct {
+	ID       int32  `json:"id"`
+	Question string `json:"question"`
+	Answer   string `json:"answer"`
+	TestID   int32  `json:"test_id"`
+}
