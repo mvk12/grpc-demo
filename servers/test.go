482cd3678e8ef059d0c718dc931eb6830521a7e1
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2025-12-12 17:05:19 -0600

    feat: agregar método CreateQuestions en TestServer para manejar la creación de preguntas a través de gRPC

--- servers/test.go
+++ servers/test.go
@@ -2,6 +2,7 @@ package servers
 
 import (
 	"context"
+	"io"
 
 	"github.com/mvk12/grpc-demo/models"
 	"github.com/mvk12/grpc-demo/pb"
@@ -42,4 +43,32 @@ func (s *TestServer) CreateTest(ctx context.Context, req *pb.Test) (*pb.TestResp
 	return &pb.TestResponse{
 		Id: test.ID,
 	}, nil
+}
+
+func (s *TestServer) CreateQuestions(stream pb.TestService_CreateQuestionsServer) error {
+	for {
+		req, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.QuestionsResponse{
+				Ok: true,
+			})
+		}
+
+		if err != nil {
+			return err
+		}
+
+		questionModel := &models.Question{
+			Question: req.GetQuestion(),
+			Answer:   req.GetAnswer(),
+			TestID:   req.GetTestId(),
+		}
+
+		_, err = s.repo.CreateQuestion(stream.Context(), questionModel)
+		if err != nil {
+			return stream.SendAndClose(&pb.QuestionsResponse{
+				Ok: false,
+			})
+		}
+	}
 }
