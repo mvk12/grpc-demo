418ae024548437e7c597a33e19c5b10b04734e0b
Miguel Angel Vázquez Vázquez <mvk_12@outlook.com>
2025-12-12 12:39:54 -0600

    feat: agregar tabla tests en la base de datos

--- database/01.start.sql
+++ database/01.start.sql
@@ -10,3 +10,14 @@ CREATE TABLE public.students
     "email_lower" VARCHAR(100) GENERATED ALWAYS AS (lower("email")) STORED,
     UNIQUE ("email_lower")
 );
+
+DROP TABLE IF EXISTS public.tests;
+
+CREATE TABLE public.tests
+(
+    "id" SERIAL PRIMARY KEY,
+    "title" VARCHAR(255) NOT NULL,
+    "description" TEXT,
+    "created_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+);
